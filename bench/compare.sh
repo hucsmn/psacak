ef2bd52d4c2c7b91e159ea186451a84cfbbf3633
hucsmn <hucsmn@hotmail.com>
2020-03-16 18:32:15 +0800

    add random sample generator

--- bench/compare.sh
+++ bench/compare.sh
@@ -6,13 +6,15 @@ time_log="result/compare_$(date +"%s")"
 rss_log="${time_log}_rss.tsv"
 time_log="${time_log}_time.tsv"
 
-echo bin/* | sed 's:\s*bin/:\t:g' | tee "$time_log" "$rss_log" > /dev/null
-for data in data/*; do
-  echo -ne "${data#data/}\t" >> "$time_log"
-  echo -ne "${data#data/}\t" >> "$rss_log"
-  for bin in bin/*; do
-    echo -n "benchmark ${bin#bin/} ${data#data/} ... "
-    "$bin" "$data" 2>&1 | sed -nE '/time:/ { s/^\s*time:\s*([0-9.]+).*$/\1/; p }; /rss:/ { s/^\s*rss:\s*([0-9.]+).*$/\1/; p };' | sed -z 's/\n/ /g' | {
+bin_list=( $(find bin/ -maxdepth 1 -type f -executable | sort | sed 's:^bin/::g') )
+data_list=( $(find data/ -maxdepth 1 -type f -exec du -k {} + | sort -n | cut -f2 | sed 's:^data/::g') )
+echo ${bin_list[@]} | sed -E 's/^|\s+/\t/g' | tee "$time_log" "$rss_log" > /dev/null
+for data in ${data_list[@]}; do
+  echo -ne "${data}\t" >> "$time_log"
+  echo -ne "${data}\t" >> "$rss_log"
+  for bin in ${bin_list[@]}; do
+    echo -n "benchmark ${bin} ${data} ... "
+    "bin/$bin" "data/$data" 2>&1 | sed -nE '/time:/ { s/^\s*time:\s*([0-9.]+).*$/\1/; p }; /rss:/ { s/^\s*rss:\s*([0-9.]+).*$/\1/; p };' | sed -z 's/\n/ /g' | {
       read time rss;
       echo "${time}s ${rss}MiB"
       echo -ne "${time}\t" >> "$time_log"
---
+++ bench/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$(realpath "$(which "$0")")")"
+
+random_data() {
+  local length="$1"
+  local alphabet="$2"
+
+  local set0="$(for i in {0..255}; do printf "\\%o" "$i"; done)"
+  local set1="$(for i in {0..255}; do printf "\\%o" "$((i % alphabet))"; done)"
+  
+  dd status=none if=/dev/urandom bs=1M count="$length" | tr "$set0" "$set1"
+}
+
+for length in {16,64,128}; do
+  for alphabet in {4,16,128,256}; do
+    output="data/random-k${alphabet}-${length}m"
+    echo "generate $output (alphabet: ${alphabet}, size: ${length}m)"
+    random_data "$length" "$alphabet" > "$output"
+  done
+done
